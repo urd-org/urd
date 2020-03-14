9700b6c94328ac4114b10c8cafd90ee0d1c23c1f
nijynot <nijynot@gmail.com>
2020-03-15 00:55:09 +0100

    test: init consensus-test.js

---
+++ test/consensus-test.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+const assert = require('bsert');
+const consensus = require('../lib/consensus');
+
+describe('Consensus', function() {
+});
